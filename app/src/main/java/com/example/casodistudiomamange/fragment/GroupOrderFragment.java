163c17c67e36731bf42939c21ac158b7af49666d
g.diasparra1 <g.diasparra1@studenti.uniba.it>
2022-03-03 19:27:50 +0100

    Refactoring e rimozione dei warning più importanti

--- app/src/main/java/com/example/casodistudiomamange/fragment/GroupOrderFragment.java
+++ app/src/main/java/com/example/casodistudiomamange/fragment/GroupOrderFragment.java
@@ -4,42 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.casodistudiomamange.R;
 import com.example.casodistudiomamange.activity.MaMangeNavigationActivity;
 import com.example.casodistudiomamange.adapter.Adapter_Plates_Ordered;
 import com.example.casodistudiomamange.adapter.Adapter_Profile;
 import com.example.casodistudiomamange.model.Plate;
-import com.example.casodistudiomamange.model.PlateInOrder;
 import com.example.casodistudiomamange.model.Profile;
 import com.example.casodistudiomamange.model.SoPlate;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class GroupOrderFragment extends Fragment {
 
     RecyclerView recyclerView;
-
     List<Profile> profileList;
     Adapter_Profile adapter_profile;
     Adapter_Plates_Ordered adapter_plates_ordered;
     FirebaseFirestore ffdb;
     ArrayList<Plate> platesInOrder;
-
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -49,7 +37,6 @@ public class GroupOrderFragment extends Fragment {
         platesInOrder = new ArrayList<>();
         platesInOrder.add(new Plate("Ciao","EDAMAME","Piatto buono",(long)2,"ANTIPASTI"));
         adapter_plates_ordered = new Adapter_Plates_Ordered(getContext(),platesInOrder);
-
 
         adapter_profile = new Adapter_Profile(profileList,getContext(),adapter_plates_ordered);
     }
@@ -71,7 +58,6 @@ public class GroupOrderFragment extends Fragment {
         return v;
     }
 
-
     private void caricaProfileEdOrdini(){
 
         String usernameInserito = ((MaMangeNavigationActivity) getActivity()).username;
@@ -79,12 +65,11 @@ public class GroupOrderFragment extends Fragment {
         String codiceGroupOrder = ((MaMangeNavigationActivity) getActivity()).codiceGroupOrder;
         String codiceTavolo = ((MaMangeNavigationActivity) getActivity()).codiceTavolo;
 
-        ArrayList<SoPlate> soPlate = new ArrayList<SoPlate>();
+        ArrayList<SoPlate> soPlate = new ArrayList<>();
         soPlate.add(new SoPlate(codiceSingleOrder,"EDAMAME",2,codiceGroupOrder,codiceTavolo));
         // public SoPlate(String codiceSingleOrder, String nomePiatto, long quantita, String codiceGroupOrder, String codiceTavolo) {
 
         profileList.add(new Profile(usernameInserito,soPlate));
-
 
     }
 }
--- app/src/main/java/com/example/casodistudiomamange/fragment/GuestFragment.java
+++ app/src/main/java/com/example/casodistudiomamange/fragment/GuestFragment.java
@@ -3,34 +3,19 @@ package com.example.casodistudiomamange.fragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
 import com.example.casodistudiomamange.R;
-import com.example.casodistudiomamange.activity.LoggedUser;
 import com.example.casodistudiomamange.activity.QRCodeActivity;
-import com.example.casodistudiomamange.activity.SwitchLoginSignupGuestActivity;
-import com.example.casodistudiomamange.model.Table;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
 
 public class GuestFragment extends Fragment {
 
     private static final int MAX_LENGTH = 10;
-    private Button unirsiTavolo;
     private EditText tw_username;
 
     @Override
@@ -39,7 +24,7 @@ public class GuestFragment extends Fragment {
         // Inflate the layout for this fragment
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_guest, container, false);
 
-        unirsiTavolo = root.findViewById(R.id.uniscitiGroupOrder);
+        Button unirsiTavolo = root.findViewById(R.id.uniscitiGroupOrder);
         tw_username=root.findViewById(R.id.username);
 
         unirsiTavolo.setOnClickListener(new View.OnClickListener() {
@@ -48,21 +33,17 @@ public class GuestFragment extends Fragment {
                 uniscitiAlTavolo();
             }
         });
-
         return root;
     }
 
     private void uniscitiAlTavolo(){
         String username_ins=tw_username.getText().toString();
-        DatabaseReference dataref_guest;
 
         if(username_ins.length()==0 || username_ins.length()>MAX_LENGTH){
             AlertDialog.Builder builder1 = new AlertDialog.Builder(getActivity());
             builder1.setTitle(R.string.username_nonvalido);
             builder1.setMessage(R.string.username_nonvalido_descr);
-
             builder1.setCancelable(true);
-
             builder1.setPositiveButton(
                     "Ok",
                     new DialogInterface.OnClickListener() {
@@ -78,6 +59,5 @@ public class GuestFragment extends Fragment {
             intent.putExtra("UsernameInserito",username_ins);
             startActivity(intent);
         }
-
     }
 }
