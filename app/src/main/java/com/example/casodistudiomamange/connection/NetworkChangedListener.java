eb13619145cbfe669d39055d742fa3bc8299168f
miche <michelediru14@gmail.com>
2022-03-23 20:26:35 +0100

    Implementazione grafica del dialog che avvisa che non c'è connessione

--- app/src/main/java/com/example/casodistudiomamange/connection/NetworkChangedListener.java
+++ app/src/main/java/com/example/casodistudiomamange/connection/NetworkChangedListener.java
@@ -1,12 +1,18 @@
 package com.example.casodistudiomamange.connection;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
@@ -15,24 +21,27 @@ import com.example.casodistudiomamange.R;
 
 public class NetworkChangedListener extends BroadcastReceiver {
 
+    View layout_dialog;
+
     @Override
     public void onReceive(Context context, Intent intent) {
         if (!Common.isConnectedToInternet(context)) {
             //internet is not connected
-            AlertDialog.Builder builder = new AlertDialog.Builder(context);
-            View layout_dialog = LayoutInflater.from(context).inflate(R.layout.check_internet_dialog, null);
-            builder.setView(layout_dialog);
 
-            AppCompatButton btnRetry = layout_dialog.findViewById(R.id.buttonretry);
 
-            //Show dialog
+            Dialog dialog = new Dialog(context, android.R.style.Theme_Dialog);
 
-            AlertDialog dialog = builder.create();
-            dialog.show();
-            dialog.setCancelable(false);
-
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setContentView(LayoutInflater.from(context).inflate(R.layout.check_internet_dialog, null));
+            dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+            dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
             dialog.getWindow().setGravity(Gravity.CENTER);
-
+            dialog.getWindow().setLayout(900,900);
+            dialog.show();
+            dialog.getWindow().setGravity(Gravity.CENTER);
+            dialog.setCancelable(false);
+            AppCompatButton btnRetry = dialog.findViewById(R.id.buttonretry);
             btnRetry.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
